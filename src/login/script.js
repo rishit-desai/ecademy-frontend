bf6184e9ee94193f163dcb03c4af898ac3fbcf8d
parnavh <parnav100sach@gmail.com>
2022-03-23 23:30:52 +0530

    login register

--- src/login/script.js
+++ src/login/script.js
@@ -1,55 +1,84 @@
-import './style.scss'
-import 'https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.8.2/angular.min.js'
+import "./style.scss";
+import "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.8.2/angular.min.js";
+import Joi from "joi";
 
-function navigation_slide()
-{
-    const burger = document.querySelector('.burger');
-    const navigation = document.querySelector('.links');
-    const navigation_links = document.querySelectorAll('.links li');
+function navigation_slide() {
+	const burger = document.querySelector(".burger");
+	const navigation = document.querySelector(".links");
+	const navigation_links = document.querySelectorAll(".links li");
 
-    burger.addEventListener('click', () =>
-    {
-        if (navigation.getAttribute("data-state") == "active")
-        {
-            navigation.setAttribute("data-state", "inactive");
-        } else
-        {
-            navigation.setAttribute("data-state", "active");
-        }
+	burger.addEventListener("click", () => {
+		if (navigation.getAttribute("data-state") == "active") {
+			navigation.setAttribute("data-state", "inactive");
+		} else {
+			navigation.setAttribute("data-state", "active");
+		}
 
-        navigation_links.forEach((link, index) =>
-        {
-            if (link.style.animation)
-            {
-                link.style.animation = ""
-            } else
-                link.style.animation = `naviagtionLinkFade 0.5s ease forwards ${index / 5 + 0.3}s`
-        })
+		navigation_links.forEach((link, index) => {
+			if (link.style.animation) {
+				link.style.animation = "";
+			} else
+				link.style.animation = `naviagtionLinkFade 0.5s ease forwards ${index / 5 + 0.3}s`;
+		});
 
-        burger.classList.toggle('toggle');
-    })
+		burger.classList.toggle("toggle");
+	});
 }
 
-function navigation_shadow()
-{
-    var navbar = document.querySelector(".primary-header");
-    if (window.pageYOffset > 0)
-    {
-        navbar.classList.add("shadow")
-    } else
-    {
-        navbar.classList.remove("shadow");
-    }
+function register_angular() {
+    const angular_app = angular.module("loginModule", []).controller("loginController", function ($scope, $http) {
+        const test_schema = Joi.object({
+            email: Joi.string().email({tlds: {allow: false}}).required(),
+            password: Joi.string().regex(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/).required()
+        })
+
+        $scope.login = function () {
+            const result = test_schema.validate({
+                email: $scope.email,
+                password: $scope.password
+            }, {abortEarly: false});
+            if (result.error) {
+                result.error.details.forEach(err => {
+                    if (err.path[0] == "email")
+                        $scope.email_error = "Please enter a valid email" 
+                    else
+                        $scope.password_error = "Please enter a valid password containing at least 8 characters, one uppercase, one lowercase, one number and one special character"
+                })
+            } else {
+                try {
+                    $http.post("http://localhost:6174/api/auth/login", {
+                        email: $scope.email,
+                        password: $scope.password
+                    }).then(res => {
+                        alert("Logged In!")
+                        window.localStorage.setItem("user", res.data.user)
+                        window.location.href = "/"
+                    }, res => {
+                        if (res.data.code == 0) {
+                            alert("Invalid email or password");
+                        } else if (res.data.code == 1) {
+                            alert("Wrong password, try again");
+                        } else {
+                            alert("Something went wrong");
+                        }
+                    });
+                } catch (ignored) {}
+            }
+        }
+
+        $scope.remove_error = function (type) {
+            eval(`$scope.${type}_error = ""`);
+        }
+    });
 }
 
 /**
  * Initialises all the functions.
- * Add your function which needs to run at start to this function 
+ * Add your function which needs to run at start to this function
  */
-async function initialise()
-{
-    navigation_slide();
-    window.onscroll = () => navigation_shadow();
+async function initialise() {
+	navigation_slide();
+    register_angular();
 }
 
-initialise()
+initialise();
--- src/register/script.js
+++ src/register/script.js
@@ -1,8 +1,8 @@
 import './style.scss'
 import 'https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.8.2/angular.min.js'
+import Joi from 'joi';
 
-function navigation_slide()
-{
+function navigation_slide() {
     const burger = document.querySelector('.burger');
     const navigation = document.querySelector('.links');
     const navigation_links = document.querySelectorAll('.links li');
@@ -30,26 +30,78 @@ function navigation_slide()
     })
 }
 
-function navigation_shadow()
-{
-    var navbar = document.querySelector(".primary-header");
-    if (window.pageYOffset > 0)
-    {
-        navbar.classList.add("shadow")
-    } else
-    {
-        navbar.classList.remove("shadow");
-    }
+function register_angular() {
+    const angular_app = angular.module("registerModule", []).controller("registerController", function ($scope, $http) {
+        const test_schema = Joi.object({
+            profession: Joi.string().required(),
+            name: Joi.string().required(),
+            email: Joi.string().email({tlds: {allow: false}}).required(),
+            password: Joi.string().regex(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/).required()
+        })
+
+        $scope.register = function () {
+
+            const data = {
+                profession: $scope.profession,
+                name: $scope.name,
+                email: $scope.email,
+                password: $scope.password
+            }
+
+            const result = test_schema.validate(data, {abortEarly: false});
+
+            if (result.error) {
+                result.error.details.forEach(err => {
+                    if (err.path[0] == "email")
+                        $scope.email_error = "Please enter a valid email" 
+                    else if (err.path[0] == "password")
+                        $scope.password_error = "Please enter a valid password containing at least 8 characters, one uppercase, one lowercase, one number and one special character"
+                    else if (err.path[0] == "name")
+                        $scope.name_error = "Please enter your name"
+                    else 
+                        $scope.profession_error = "Please enter a profession"
+                })
+            } else {
+                $http({
+                    method: 'POST',
+                    url: 'http://localhost:6174/api/auth/register',
+                    headers: {
+                        'Content-Type': "application/json"
+                    },
+                    data
+                }).then(res => console.log('success', res), res => alert(res.data.reason))
+
+                $http.post("http://localhost:6174/api/auth/register", {
+                    data
+                }).then(res => {
+                    alert("Registered!")
+                    window.localStorage.setItem("user", res.data.user)
+                    window.location.href = "/"
+                }, res => {
+                    if (res.data.code == 0) {
+                        alert("Something went wrong");
+                    } else if (res.data.code == 1) {
+                        alert("Email already in use, try loging in");
+                    } else {
+                        alert("Something went wrong");
+                    }
+                });
+            }
+        }
+
+        $scope.remove_error = function (type) {
+            eval(`$scope.${type}_error = ""`);
+        }
+    });
 }
 
 /**
  * Initialises all the functions.
  * Add your function which needs to run at start to this function 
  */
-async function initialise()
-{
+async function initialise() {
     navigation_slide();
-    window.onscroll = () => navigation_shadow();
+    register_angular();
 }
 
 initialise()
